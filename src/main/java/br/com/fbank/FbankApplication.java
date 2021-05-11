e809e5287c79d58065f03bfc31af2e8fda7e01b0
joaomarcosfelixds <joaomarcosfelixds@gmail.com>
2021-05-10 21:28:44 -0300

    Starting project

---
+++ src/main/java/br/com/fbank/FbankApplication.java
@@ -0,0 +1,13 @@
+package br.com.fbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fbank/FbankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
